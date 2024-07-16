4469b662b0ff9ce062480571966db1dc8337a571
MehulBafna <mehulbafna198@gmail.com>
2024-07-16 19:36:40 +0200

    Final commit

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -81,8 +81,8 @@ class DataTransformation:
     def initiate_data_transformation(self,train_path,test_path):
 
         try:
-            train_df=pd.read_csv(r"C:\\Users\\Mehul\BlueYonderTask\\artifacts\\train.csv")
-            test_df=pd.read_csv(r"C:\\Users\\Mehul\\BlueYonderTask\\artifacts\\test.csv")
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
 
             logging.info("Read train and test data completed")
 
