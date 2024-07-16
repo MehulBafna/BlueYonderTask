2526c0bd35f7b5b78d073d4cef4f9d503a371387
MehulBafna <mehulbafna198@gmail.com>
2024-07-16 14:52:15 +0200

    Flask Deployment

---
+++ app.py
@@ -0,0 +1,49 @@
+import pickle 
+from flask import Flask, request,render_template
+import numpy as np 
+import pandas as pd 
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+application=Flask(__name__)
+
+app = application 
+
+## Route for a Home Page 
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            season=int(request.form.get('season')),
+            year=int(request.form.get('year')),
+            hour=int(request.form.get('hour')),
+            holiday=int(request.form.get('holiday')),
+            weekday=int(request.form.get('weekday')),
+            working_day=int(request.form.get('working_day')),
+            weather_situation=int(request.form.get('weather_situation')),
+            temperature=float(request.form.get('temperature')),
+            feels_like_temperature=float(request.form.get('feels_like_temperature')),
+            humidity=float(request.form.get('humidity')),
+            wind_speed=float(request.form.get('wind_speed')),
+            casual=int(request.form.get('casual')),
+            registered=int(request.form.get('registered')),
+            i_hour=int(request.form.get('i_hour'))
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
+if __name__=="__main__":
+    app.run(host="0.0.0.0",debug=True)
---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,99 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+import os
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+
+class CustomData:
+    def __init__(self,
+        season: int,
+        year: int,
+        hour: int,
+        holiday: int,
+        weekday: int,
+        working_day: int,
+        weather_situation: int,
+        temperature: float,
+        feels_like_temperature: float,
+        humidity: float,
+        wind_speed:float,
+        casual: int,
+        registered: int,
+        i_hour: int):
+
+        self.season = season
+
+        self.year = year
+
+        self.hour = hour
+
+        self.holiday = holiday
+
+        self.weekday = weekday
+
+        self.working_day = working_day
+
+        self.weather_situation = weather_situation
+
+        self.temperature = temperature
+
+        self.feels_like_temperature = feels_like_temperature 
+
+        self.humidity = humidity 
+
+        self.wind_speed = wind_speed 
+
+        self.casual = casual 
+
+        self.registered = registered 
+
+        self.i_hour = i_hour
+
+
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "season": [self.season],
+                "year": [self.year],
+                "hour": [self.hour],
+                "holiday": [self.holiday],
+                "weekday": [self.weekday],
+                "working_day": [self.working_day],
+                "weather_situation": [self.weather_situation],
+                "temperature": [self.temperature],
+                "feels_like_temperature":[self.feels_like_temperature],
+                "humidity": [self.humidity],
+                "wind_speed": [self.wind_speed],
+                "casual": [self.casual],
+                "registered": [self.registered],
+                "i_hour": [self.i_hour]
+
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
