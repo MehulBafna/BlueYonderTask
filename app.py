f404ebe55ab2d793fb5ae279c57c9e794c43b314
MehulBafna <mehulbafna198@gmail.com>
2024-07-17 12:42:02 +0200

    render cloud deployment

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request,render_template
 import numpy as np 
 import pandas as pd 
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+import gunicorn
 
 application=Flask(__name__)
 
